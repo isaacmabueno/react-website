dc618d13726ee7b6ddd5270e47bc27de38e4a2ef
Isaac Bueno <isaacbueno@Isaacs-MacBook-Pro.local>
2019-11-10 20:56:51 -0600

    updating header section for rubens website;

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 // import Yelp from './Components/Yelp';
 import Header from './Components/Header'
+import Nav from './Components/Nav'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <Header />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
