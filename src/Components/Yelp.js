1e2ea464cc97c983634b3081e3bd422251e56a51
Isaac Bueno <isaacbueno@Isaacs-MacBook-Pro.local>
2019-11-22 20:34:59 -0600

    updating api call

--- src/Components/Yelp.js
+++ src/Components/Yelp.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import axios from 'axios'
 
+const config = {
+    headers: {'Authorization': 'Juyt4HDbaXyPRnY6Y1hZXTuC85vgkwGhD2tiDSIKZ1B8bJ6xkqiZZIq2WiD0SMFF95MhAcoQv-J7kikK3MZZNHh2zfxNBDANcjENuPtmPSGNr0NBykA-tk1sWNGKW3Yx'},
+    params: {
+      term: 'tacos',
+      location: 'main 123st'
+    }
+  };
 export default class Yelp extends React.Component {
+   
+
     state = {
         reviews: [],
     };
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(res=> {
+        axios.get('https://api.yelp.com/v3/businesses/search', config)
+        .then(res=> {
             console.log(res);
-            this.setState({ reviews :res.data });
+            // this.setState({ reviews :res.data });
         });
     }
+    
+
+    
     render() {
         return(
             <div className="yelp-reviews">
@@ -22,7 +35,9 @@ export default class Yelp extends React.Component {
                                 <h5 className="card-title">{review.title}</h5>
                                 <p className="card-text">{review.title}</p>
                                 <p className="card-text"><small class="text-muted">{review.id}</small></p>
+                                
                             </div>
+                           
                         </div>
                     )}
                 </ul>
@@ -30,4 +45,3 @@ export default class Yelp extends React.Component {
         )
     }
 }
-
